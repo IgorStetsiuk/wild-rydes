aa4a4f2a85e0bc7da2182090c8f08f0a2f63b857
Igor Stetsiuk <igorstetsuk@gmail.com>
2020-04-30 20:32:03 +0300

    configured user-pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_lJ4lX7uTL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3mmjc7h4ljo2mnaco8429dt7cm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
