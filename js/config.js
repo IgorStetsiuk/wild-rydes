17ca5eedcc92ad7aa3c384849c3cf19cf584ff64
Igor Stetsiuk <igorstetsuk@gmail.com>
2020-04-30 20:40:15 +0300

    Redeploying key

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-central-1_lJ4lX7uTL', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3mmjc7h4ljo2mnaco8429dt7cm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2rogrm7ksovjmq5ht1iek9pqjn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
